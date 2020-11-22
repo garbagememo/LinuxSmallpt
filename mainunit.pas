9af97b6ec605682f2bff864a12d2703fdab6d0b1
garbagememo <66831242+garbagememo@users.noreply.github.com>
2020-11-22 20:55:32 +0900

    Add files via upload

---
+++ mainunit.pas
@@ -0,0 +1,168 @@
+{
+ ***************************************************************************
+ *                                                                         *
+ *   This source is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This code is distributed in the hope that it will be useful, but      *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   A copy of the GNU General Public License is available on the World    *
+ *   Wide Web at <http://www.gnu.org/copyleft/gpl.html>. You can also      *
+ *   obtain it by writing to the Free Software Foundation,                 *
+ *   Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1335, USA.   *
+ *                                                                         *
+ ***************************************************************************
+
+  Abstract:
+    Demo to show, how to start a thread and how synchronize with the main
+    thread.
+    Important: The cthread unint must be added to the uses section of the .lpr
+               file. See multithreadingexample1.lpr.
+}
+unit MainUnit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  uVect;
+
+type
+
+  { TMyThread }
+  TTileBuffer=array[0..255,0..255] of VecRecord;
+
+  TMyThread = class(TThread)
+  private
+    fStatusText: string;
+    DoneCalc:boolean;
+    procedure ShowStatus;
+  protected
+    procedure Execute; override;
+  public
+    TileBuffer:TTileBuffer;
+    constructor Create(CreateSuspended: boolean);
+  end;
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    cmdRender: TButton;
+    StrWidth: TEdit;
+    StrHeight: TEdit;
+    StrSampleCount: TEdit;
+    StrThreadCount: TEdit;
+    ImgRender: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    procedure cmdRenderClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+  public
+  end;
+
+    
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+  DoubleBuffered := TRUE;
+  TOP:=10;
+  Left:=10;
+end;
+
+procedure TMainForm.cmdRenderClick(Sender: TObject);
+var
+  MyThread : TMyThread;
+begin
+  imgRender.Width := strtoint(strWidth.Text);
+  imgRender.Height := strtoint(strHeight.Text);
+//add
+  imgRender.Picture.Bitmap.Width:=imgRender.Width;
+  imgRender.Picture.Bitmap.Height:=imgRender.Height;
+//Orginal source
+  imgRender.Canvas.Brush.Color := clBlack;
+  imgRender.Canvas.FillRect(0,0, imgRender.Width, imgRender.Height);
+
+  cmdRender.Enabled:=FALSE;
+  ClientWidth := imgRender.Left + 5 + imgRender.Width;
+  ClientHeight := imgRender.Top + 5 + imgRender.Height;
+
+  MyThread := TMyThread.Create(True); // With the True parameter it doesn't start automatically
+  if Assigned(MyThread.FatalException) then
+    raise MyThread.FatalException;
+      
+  // Here the code initialises anything required before the threads starts executing
+
+  MyThread.Start;
+
+end;
+
+{ TMyThread }
+
+procedure TMyThread.ShowStatus;
+// this method is only called by Synchronize(@ShowStatus) and therefore
+// executed by the main thread
+// The main thread can access GUI elements, for example MainForm.Caption.
+var
+   x,y : integer;
+begin
+  MainForm.Caption := fStatusText;
+  IF DoneCalc THEN BEGIN
+     FOR y:=0 to 255 do begin
+	FOR x:=0 to 255 do begin
+	   MainForm.ImgRender.Canvas.Pixels[x,y]:= Trunc(TileBuffer[x,y].x)+
+                                                   Trunc(TileBuffer[x,y].y)*256+
+                                                   Trunc(TileBuffer[x,y].z)*256*256;
+	END;
+     END;
+  END;
+  
+end;
+
+procedure TMyThread.Execute;
+var
+  newStatus : string;
+   x,y	    : integer;
+begin
+  fStatusText := 'TMyThread Starting ...';
+  Synchronize(@Showstatus);
+  fStatusText := 'TMyThread Running ...';
+  Synchronize(@ShowStatus);
+  for y:=0 to 255 do begin
+    for x:=0 to 255 do begin
+      TileBuffer[x,y].x:=y;
+      TileBuffer[x,y].y:=x;
+      TileBuffer[x,y].z:=128;
+    END;
+  END;
+    newStatus:='TMyThread Time: '+FormatDateTime('YYYY-MM-DD HH:NN:SS',Now);
+    DoneCalc:=TRUE;
+    Synchronize(@ShowStatus);
+end;
+
+constructor TMyThread.Create(CreateSuspended: boolean);
+begin
+  FreeOnTerminate := True;
+   DoneCalc:=FALSE;
+  inherited Create(CreateSuspended);
+end;
+
+end.
+
---
+++ uVect.pas
@@ -0,0 +1,190 @@
+﻿UNIT uVect;
+{$MODE objfpc}{$H+}
+{$INLINE ON}
+
+INTERFACE
+
+USES
+    sysutils,math;
+TYPE
+    RefType=(DIFF,SPEC,REFR);// material types, used in radiance()
+{
+	DIFFUSE,    // 完全拡散面。いわゆるLambertian面。
+	SPECULAR,   // 理想的な鏡面。
+	REFRACTION, // 理想的なガラス的物質。
+}
+    VecRecord=Record
+        x,y,z:real;
+    end;
+    RayRecord=Record
+       o, d:VecRecord;
+     end;
+   function CreateRay(o_,d_:VecRecord):RayRecord;
+
+   function ClampVector(v:VecRecord):VecRecord;
+const
+   BackGroundColor:VecRecord = (x:0;y:0;z:0);
+   ZeroVec:VecRecord = (x:0;y:0;z:0);
+   
+function CreateVec(x_,y_,z_:real):VecRecord;
+FUNCTION VecMul(const V1,V2:VecRecord):VecRecord;inline;
+FUNCTION VecNeg(V:VecRecord):VecRecord;
+FUNCTION Veclen(V:VecRecord):real;inline;
+FUNCTION VecNorm(V:VecRecord):VecRecord;inline;
+FUNCTION VecDot(const V1,V2 :VecRecord):real;//内積
+FUNCTION VecCross(const V1,V2 :VecRecord):VecRecord;//外積
+FUNCTION VecAdd3(V1,V2,V3:VecRecord):VecRecord;
+procedure VecWriteln(V:VecRecord);
+
+operator * (const v1:VecRecord;const r:real)v:VecRecord;inline;
+operator / (const v1:VecRecord;const r:real)v:VecRecord;inline;
+operator * (const v1,v2:VecRecord)r:real;inline;//内積
+operator / (const v1,v2:VecRecord)v:VecRecord;inline;//外積
+
+operator + (const v1,v2:VecRecord)v:VecRecord;inline;
+operator - (const v1,v2:VecRecord)v:VecRecord;inline;
+operator + (const v1:VecRecord;const r:real)v:VecRecord;inline;
+operator - (const v1:VecRecord;const r:real)v:VecRecord;inline;
+
+function ColToByte(x:real):byte;inline;
+
+IMPLEMENTATION
+
+function CreateVec(x_,y_,z_:real):VecRecord;
+BEGIN
+    result.x:=x_;result.y:=y_;result.z:=z_;
+END;
+function CreateRay(o_,d_:VecRecord):RayRecord;
+begin
+    result.o:=o_;
+    result.d:=d_;
+end;
+
+FUNCTION VecMul(const V1,V2:VecRecord):VecRecord;inline;
+BEGIN
+    result.x:=V1.x*V2.x;
+    result.y:=V1.y*V2.y;
+    result.z:=V1.z*V2.z;
+END;
+
+FUNCTION VecNeg(V:VecRecord):VecRecord;
+BEGIN
+    result.x:=-V.x;
+    result.y:=-V.y;
+    result.z:=-V.z;
+END;
+FUNCTION Veclen(V:VecRecord):real;inline;
+BEGIN
+   result:=sqrt(V.x*V.x+V.y*V.y+V.z*V.z);
+END;
+
+FUNCTION VecNorm(V:VecRecord):VecRecord;inline;
+BEGIN
+    result:=V/VecLen(V) ;
+END;
+FUNCTION VecDot(const V1,V2 :VecRecord):real;//内積
+BEGIN
+    result:=v1.x*v2.x+v1.y*v2.y+v1.z*v2.z;
+END;
+FUNCTION VecCross(const V1,V2 :VecRecord):VecRecord;//外積
+BEGIN
+    result.x:=V1.y * v2.z - v2.y * V1.z;
+    result.y:=V1.z * v2.x - v2.z * V1.x;
+    result.z:=V1.x * v2.y - v2.x * V1.y;
+END;
+FUNCTION VecAdd3(V1,V2,V3:VecRecord):VecRecord;
+BEGIN
+    result.x:=V1.x+V2.x+V3.x;
+    result.y:=V1.y+V2.y+V3.y;
+    result.z:=V1.z+V2.z+V3.z;
+    
+END;
+function FtoSF(r:real):string;
+var
+  i,j:LongInt;
+begin
+  i:=5;j:=5;
+  result:=FloatToStrf(r,ffFixed,I,J);
+end;
+
+procedure VecWriteln(V:VecRecord);
+begin
+    Writeln(FtoSF(v.x),' : ',FtoSF(v.y),' : ',FtoSF(v.z));
+end;
+
+
+operator * (const v1:VecRecord;const r:real)v:VecRecord;inline;
+begin
+   v.x:=v1.x*r;
+   v.y:=v1.y*r;
+   v.z:=v1.z*r;
+end;
+
+operator / (const v1:VecRecord;const r:real)v:VecRecord;inline;
+begin
+   v.x:=v1.x/r;
+   v.y:=v1.y/r;
+   v.z:=v1.z/r;
+end;
+
+operator * (const v1,v2:VecRecord)r:real;inline;//内積
+begin
+   r:=v1.x*v2.x+v1.y*v2.y+v1.z*v2.z;
+end;
+
+operator / (const v1,v2:VecRecord)v:VecRecord;inline; //外積
+begin
+    v.x:=V1.y * v2.z - v2.y * V1.z;
+    v.y:=V1.z * v2.x - v2.z * V1.x;
+    v.z:=V1.x * v2.y - v2.x * V1.y;
+end;
+
+operator + (const v1,v2:VecRecord)v:VecRecord;inline;
+begin
+   v.x:=v1.x+v2.x;
+   v.y:=v1.y+v2.y;
+   v.z:=v1.z+v2.z;
+end;
+
+operator - (const v1,v2:VecRecord)v:VecRecord;inline;
+begin
+    v.x:=v1.x-v2.x;
+    v.y:=v1.y-v2.y;
+    v.z:=v1.z-v2.z;
+end;
+
+operator + (const v1:VecRecord;const r:real)v:VecRecord;inline;
+begin
+   v.x:=v1.x+r;
+   v.y:=v1.y+r;
+   v.z:=v1.z+r;
+end;
+operator - (const v1:VecRecord;const r:real)v:VecRecord;inline;
+begin
+    v.x:=v1.x-r;
+    v.y:=v1.y-r;
+    v.z:=v1.z-r;
+end;
+
+
+function Clamp(x:real):real;inline;
+begin
+   IF x<0 then exit(0);
+   IF x>1 then exit(1);
+   exit(x);
+end;
+
+function ClampVector(v:VecRecord):VecRecord;
+begin
+  result.x:=clamp(v.x);
+  result.y:=clamp(v.y);
+  result.z:=clamp(v.z);
+end;
+function ColToByte(x:real):byte;inline;
+begin
+    result:=trunc(power(clamp(x),1/2.2)*255+0.5);
+end;
+
+BEGIN
+END.
+
